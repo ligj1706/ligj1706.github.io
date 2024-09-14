4d316048a6cf02c781b7a1eac0fb47352ec59cbb
ligj1706 <2095652244@qq.com>
2024-09-15 00:49:57 +0800

    Site updated: 2024-09-15 00:49:56

--- js/next-boot.js
+++ js/next-boot.js
@@ -109,14 +109,6 @@ NexT.boot.motion = function() {
 
 document.addEventListener('DOMContentLoaded', () => {
   NexT.boot.registerEvents();
-  
-  // 添加切换背景的逻辑
-  const toggleButton = document.getElementById('toggle-theme');
-  if (toggleButton) {
-    toggleButton.addEventListener('click', () => {
-      document.body.classList.toggle('dark-mode');
-    });
-  }
 
   NexT.boot.refresh();
   NexT.boot.motion();
